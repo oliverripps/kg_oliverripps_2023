7f6157ec14e885fb74d0291bc0c4b8fa28a51890
Oliver Ripps <oliver.ripps@com>
2021-04-26 21:30:45 -0400

    number is parsed as well, giving correct output

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
+const parseNumber = (num) => {
+  let digitList = new Array();
+  let firstDigit = true;
+  while (num >= 1 || firstDigit) {
+    if (num % 10 === 0) {
+      digitList.push('Zero');
+    } else if (num % 10 == 1) {
+      digitList.push('One');
+    } else if (num % 10 == 2) {
+      digitList.push('Two');
+    } else if (num % 10 == 3) {
+      digitList.push('Three');
+    } else if (num % 10 == 4) {
+      digitList.push('Four');
+    } else if (num % 10 == 5) {
+      digitList.push('Five');
+    } else if (num % 10 == 6) {
+      digitList.push('Six');
+    } else if (num % 10 == 7) {
+      digitList.push('Seven');
+    } else if (num % 10 == 8) {
+      digitList.push('Eight');
+    } else {
+      digitList.push('Nine');
+    }
+    firstDigit = false;
+    num = parseInt(num / 10);
+  }
+  let digitString = '';
+  for (let i = digitList.length - 1; i >= 0; i--) {
+    digitString += digitList[i];
+  }
+  return digitString;
+};
 const parseInput = (input) => {
-  let numList = new Array();
+  let numList = '';
   for (let i = 0; i < input.length; i++) {
-    numList.push(input[i]);
+    numList += parseNumber(input[i]);
+    if (i != input.length - 1) {
+      numList += ',';
+    }
   }
   return numList;
 };
